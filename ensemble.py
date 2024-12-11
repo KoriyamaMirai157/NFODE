08e7faa4aaf2920fa2d3f277b8fcf30683e65477
KuriyamaMirai <127531483+KoriyamaMirai157@users.noreply.github.com>
2024-12-11 11:25:22 +0800

    Add files via upload

---
+++ ensemble.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import nfode
+import torch.jit
+import random
+from datetime import datetime
+
+data = np.load('/home/liyilin/nfode/uq/data_set.npz')
+t_data = data['t_data']
+x_data = data['x_data']
+
+device = torch.device('cuda:0')
+x_data = torch.tensor(x_data, dtype=torch.float32).requires_grad_(True).to(device)
+t_data = torch.tensor(t_data, dtype=torch.float32).requires_grad_(True).to(device)
+
+test_data = np.load('/home/liyilin/nfode/uq/data_test.npz')
+t_data_test = test_data['t_data']
+x_data_test = test_data['x_data']
+
+input_dim=2
+output_dim=2
+inner_dim=64
+class ODEModel(nn.Module):
+    def __init__(self):
+        super(ODEModel, self).__init__()
+        self.fc1 = nn.Linear(input_dim, inner_dim)
+        self.fc2 = nn.Linear(inner_dim, inner_dim)
+        self.fc3 = nn.Linear(inner_dim, output_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+k = 1
+n = 1
+
+error_matrix = []
+params_matrix = []
+epochs_vector = []
+time_spent_vector = []
+while n <= 300:
+    model = ODEModel()
+    nfode.initialize_params_uniform(model)
+    model, epoch, time_spent = nfode.train(model, device, t_data, x_data, threshold=0.008, k=n, lr=0.01, milestones=[120], gamma=0.1)
+
+    if epoch < 600:
+        #采集参数
+        params, flat_params, param_types, param_shapes = nfode.flatten_params(model)
+        params_matrix.append(flat_params)
+
+        #采集测试集误差
+        x0 = x_data_test[:,0]
+        x_preds_test = nfode.predict(model, t_data_test, x0)
+        x_error = x_data_test - x_preds_test.detach().numpy().T
+        error_matrix.append(x_error)
+
+        #采集训练用时
+        epochs_vector.append(epoch)
+        time_spent_vector.append(time_spent)
+
+        n += 1
+
+params_matrix = np.stack(params_matrix, axis = 0)
+error_matrix = np.stack(error_matrix, axis = 2)
+epochs_vector = np.array(epochs_vector)
+time_spent_vector = np.array(time_spent_vector)
+
+np.save('/home/liyilin/nfode/uq/params_matrix.npy', params_matrix)
+np.save('/home/liyilin/nfode/uq/error_matrix.npy', error_matrix)
+np.save('/home/liyilin/nfode/uq/epochs_vector.npy', epochs_vector)
+np.save('/home/liyilin/nfode/uq/time_spent_vector.npy', time_spent_vector)
--- nfode.py
+++ nfode.py
@@ -3,46 +3,12 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import copy
-from datetime import datetime
-
-# 导函数与时间有关的Euler_Step
-def euler_step(model, ti, x_last, dt):
-    dx = model(ti, x_last)
-    x_next = x_last + dx * dt
-    return x_next
 
 # 导函数与时间无关的Euler_Step
-def euler_step2(model, dt, x_last, m=1):
+def euler_step(model, dt, x_last, m=1):
     dx = model(x_last)
     x_next = x_last + dx * dt
     return x_next
-
-def vdpo(x, m=1):
-    dx1 = m*(x[0] - 1/3*x[0]**3 -x[1])
-    dx2 = 1/m*x[0]
-    return np.array([dx1, dx2])
-
-
-# 定义神经网络模型
-class ODEModel(nn.Module):
-    def __init__(self):
-        super(ODEModel, self).__init__()
-        self.fc1 = nn.Linear(3, 64)  # 输入为 x1, x2 和 t
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 2)  # 输出为 dx1 和 dx2
-
-    def forward(self, t, x):
-        if t.dim() == 0:
-            t = t.unsqueeze(0)
-        elif t.dim() == 1:
-            t = t.unsqueeze(1)
-        elif t.size(1) > 2:
-            t = t[:, :2]
-        x = torch.cat([x, t], dim=0)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 def initialize_params_uniform(model, low=-0.2, high=0.2):
     with torch.no_grad():
@@ -50,74 +16,84 @@ def initialize_params_uniform(model, low=-0.2, high=0.2):
             # 生成均匀分布的随机数，并设置为参数
             param.uniform_(low, high)
 
-# 主训练过程
-def train(model, t_data, x_data, device, j=1, num_epochs=1200, dt=0.01, lr=0.01):
+# train函数要求输入时已经为张量，其中x_data第0维为x的维度，第1维为时间点，第2维区分不同数据样本; t_data第0维区分不同数据样本，第1维为时间点
+def train(model, device, t_data, x_data, threshold=0.008, k=1, lr=0.01, milestones=[800], gamma=0.1):
+    from datetime import datetime
+    import random
+    import copy
+    model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[800], gamma=0.2, last_epoch=-1)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=gamma, last_epoch=-1)
     loss_fn = nn.MSELoss()
-    x_data = x_data.T.clone().detach().requires_grad_(True).to(device)
-    t_data = t_data.clone().detach().requires_grad_(True).unsqueeze(1).to(device)
-    x_true_grads = []
-    for i in range(1, len(x_data)):
-        if i == 1:
-            x_true_grad = (x_data[i] - x_data[i - 1]) - (x_data[i] - x_data[i - 1])
-            x_true_grads.append(x_true_grad)
-        else:
-            x_true_grad = (x_data[i] - x_data[i - 1]) / (dt)
-            x_true_grads.append(x_true_grad)
-    x_true_grads = torch.stack(x_true_grads).squeeze()
+    x_true_grads = torch.zeros_like(x_data)
+    for i in range(1, x_data.size(1)):
+        x_true_grads[:, i, :] = (x_data[:, i, :] - x_data[:, i - 1, :]) / (t_data[:, i] - t_data[:, i - 1])
+    x_true_grads.to(device)
     loss_curve = []
     real_loss_curve = []
-    for epoch in range(num_epochs):
+    epoch = 1
+    start_time = datetime.now()
+    best_model = copy.deepcopy(model)
+    best_real_loss = 100
+    while (best_real_loss >= threshold)&(epoch<=600):
         model.train()
         optimizer.zero_grad()
-
-        # 初始状态
-        x_pred = x_data[0].clone().detach().requires_grad_(True).unsqueeze(0)
-        x_preds = [x_pred]
-        x_grads = []
-        for i in range(1, len(t_data)):
-            t = t_data[i - 1].requires_grad_(True)
-            dx = model(t, x_pred).requires_grad_(True)
-            x_pred = euler_step(model, t, x_pred, dt)
-            x_preds.append(x_pred)
-            x_grads.append(dx)
-
-        # 计算损失
-        x_preds = torch.stack(x_preds).squeeze()
-        x_grads = torch.stack(x_grads).squeeze()
-        x_true_grads = x_true_grads[:len(x_grads), :]
-        loss = loss_fn(x_grads, x_true_grads)
-        loss_curve.append(loss.cpu().detach().numpy())
-        real_loss = loss_fn(x_preds, x_data)
-        real_loss_curve.append(real_loss.cpu().detach().numpy())
-        # 反向传播
+        n = random.randint(0, x_true_grads.size(2) - 1)
+        t = t_data[n, :].to(device)
+        x = x_data[:, :, n].to(device)
+        x_preds = torch.zeros_like(x).to(device)
+        x_preds[:, 0] = x[:, 0].clone().detach().requires_grad_(True)
+        x_grads = torch.zeros_like(x).to(device)
+        for i in range(1, len(t)):
+            dt = t[i] - t[i - 1]
+            x_pred = x_preds[:, i - 1].clone()
+            dx = model(x_pred).requires_grad_(True)
+            x_pred = euler_step(model, dt, x_pred)
+            x_preds[:, i] = x_pred
+            x_grads[:, i] = dx
+        loss = loss_fn(x_grads, x_true_grads[:, :, n])
         loss.backward(retain_graph=True)
         optimizer.step()
         scheduler.step()
+        loss_curve.append(loss.cpu().detach().numpy())
+        real_loss = loss_fn(x_preds, x)
+        real_loss = real_loss.cpu().detach().numpy()
+        real_loss_curve.append(real_loss)
 
-        if (epoch + 1) % 100 == 0:
+        if real_loss <= best_real_loss:
+            best_model = copy.deepcopy(model)
+            best_real_loss = real_loss
+
+        
+        if (epoch + 1) % 10 == 0:
             current_time = datetime.now()
             current_time = current_time.strftime("%H:%M:%S")
             model_device = next(model.parameters()).device
-            print(j, epoch + 1, '/', num_epochs, current_time, model_device, 'real loss:', real_loss.detach().numpy(), 'derivate loss:', loss.detach().numpy())
-    return model, loss_curve, x_preds, real_loss_curve
+            print(k, epoch + 1, current_time, model_device, 'best real loss:', best_real_loss)
+        
+        epoch += 1
 
-def predict(model, t_data, x_data, dt):
+    best_model.to('cpu')
+    end_time = datetime.now()
+    time_spent = end_time - start_time
+    time_spent = time_spent.total_seconds()
+    return best_model, best_real_loss, epoch, time_spent
+
+#predict函数不要求输入时已经为张量, t_data为时间点(1*n维数组), x0为初值(n*1维数组)
+def predict(model, t_data, x0):
     model.eval()
-    x_data = torch.tensor(x_data, dtype=torch.float32).requires_grad_(True)
+    x0 = torch.tensor(x0, dtype=torch.float32).requires_grad_(True)
     t_data = torch.tensor(t_data, dtype=torch.float32).unsqueeze(1).requires_grad_(True)
     with torch.no_grad():
-        x_pred = x_data[0].clone().detach().unsqueeze(0)
+        x_pred = x0.clone().detach().unsqueeze(0)
         x_preds = [x_pred]
 
-        # 进行训练集内的预测
         for i in range(1, len(t_data)):
-            t = t_data[i].unsqueeze(0)
-            x_pred = euler_step(model, t, x_pred, dt)
+            dt = t_data[i] - t_data[i - 1]
+            x_pred = euler_step(model, dt, x_pred)
             x_preds.append(x_pred)
 
-    x_preds = torch.stack(x_preds).squeeze().numpy()
+    x_preds = torch.stack(x_preds)
     return x_preds
 
 def flatten_params(model):
